918d28a1dc46bd2cd3a58623d04f16a5e50575df
alexbhdez <abhbretones@gmail.com>
2021-02-17 20:28:49 +0000

    Add topbar and time with js

---
+++ js/index.js
@@ -0,0 +1,14 @@
+const showTime = () => {
+  const date = new Date()
+  const h = date.getHours()
+  const m = date.getMinutes()
+  const minutes = m < 10 ? `0${m}` : m
+  const time = `${h}:${minutes}`
+
+  const timeElement = document.querySelector('#time')
+  timeElement.innerText = time
+
+  setTimeout(showTime, 1000*60)
+}
+
+showTime()
