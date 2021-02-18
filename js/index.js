a2487fa31860a028dec96eb6c74c0a065fd90a75
alexbhdez <abhbretones@gmail.com>
2021-02-18 21:15:00 +0000

    Add basic functionality

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+  const operationElement = document.querySelector('#operation')
+  const totalElement = document.querySelector('#total')
+  const keys = document.querySelectorAll('.key')
+
+
   const showTime = () => {
     const date = new Date()
     const h = date.getHours()
@@ -13,6 +18,45 @@ document.addEventListener('DOMContentLoaded', () => {
     setTimeout(showTime, 1000*60)
   }
 
-  showTime()
+  const writeTotal = val => totalElement.innerText = val
+  const writeText = text => !text ? operationElement.innerText = '' : operationElement.innerText += text
+  const onDelete = () => operationElement.innerText = operationElement.innerText.slice(0, -1)
+  const onClear = () => totalElement.innerText = '0'
+  const onEqual = () => {
+    const val = eval(operationElement.innerText)
+    writeTotal(val)
+    operationElement.innerText = val
+  }
 
+  const action = keyElement => {
+    const { id, innerText } = keyElement
+    switch (id) {
+      case 'clear':
+        writeText('')
+        onClear()
+        break;
+      case 'delete':
+        onDelete()
+        break;
+      case 'equal':
+        onEqual()
+        break;
+      default:
+        writeText(innerText)
+        break;
+    }
+  }
+
+  const keysListener = () => {
+    keys.forEach(el => {
+      el.addEventListener('click', () => action(el))
+    })
+  }
+
+  const init = () => {
+    showTime()
+    keysListener()
+  }
+
+  init()
 })
